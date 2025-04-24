06cd8fa7d705cbfa2199983c9e50eed6b58835ed
Kirill-Bokov <kirill37reg2@gmail.com>
2025-04-24 18:57:48 +0300

    deployment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ const App = () => {
 
   useEffect(() => {
     console.log("Users updated:", users);
-    setUsers((users) => users.concat({ id: 4, name: "David" }, {id: 5, name: "JS"}));
+    setUsers((users) => users.concat({ id: 4, name: "David" }, {id: 5, name: "JS"})); //Используем concat, т.к. он создаст новый массив, что позволит избежать проблем с изменением
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // массив зависимостей оставляем пустым, т.к. в противном случае будет бесконечный рендер
+  }, []);
 
   const filteredUsers =
     filter === "" ? users : users.filter((user) => user.name === filter);
